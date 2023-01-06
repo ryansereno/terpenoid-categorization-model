8cb4a7cbf8586c3cf42473eab02cf27e2f05d754
R <ryansereno@protonmail.com>
2023-01-05 20:26:36 -0500

    added testing data sets

--- neuralnet.py
+++ neuralnet.py
@@ -5,8 +5,14 @@ import pandas as pd
 df_strains = pd.read_csv('training_data.csv')
 df_labels = pd.read_csv('training_labels.csv')
 
+td_strains = pd.read_csv('testing_data.csv')
+td_effects = pd.read_csv('testing_labels.csv')
+
 terpene_train_data = np.array(df_strains)
 effect_training_labels = np.array(df_labels)
+
+terpene_test_data = np.array(td_strains)
+effect_testing_labels = np.array(td_effects)
 
 print(terpene_train_data.shape)
 # Define the model
@@ -22,8 +28,8 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 # Fit the model to the training data
-model.fit(terpene_train_data, effect_training_labels, epochs=10, verbose='2')
+model.fit(terpene_train_data, effect_training_labels, epochs=550)
 
 # Evaluate the model on the test data
-#test_loss, test_acc = model.evaluate(X_test, y_test)
-#print('Test accuracy:', test_acc)
+test_loss, test_acc = model.evaluate(terpene_test_data, effect_testing_labels)
+print('Test accuracy:', test_acc)
